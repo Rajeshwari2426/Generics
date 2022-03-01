6cd9fdc5b82ec152196488bf67e1984eef9ad7a7
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-03-01 18:02:17 +0530

    [Rajeshwari] adding max of 3 floats

--- Generics/Generics/MaxNumber.cs
+++ Generics/Generics/MaxNumber.cs
@@ -24,5 +24,21 @@ namespace Generics
             }
             return default;
         }
+        public float GetMaxOfThree(float firstNum, float secondNum, float thirdNum)
+        {
+            if (firstNum.CompareTo(secondNum) >= 0 && firstNum.CompareTo(thirdNum) >= 0)
+            {
+                return firstNum;
+            }
+            else if (secondNum.CompareTo(firstNum) >= 0 && secondNum.CompareTo(thirdNum) >= 0)
+            {
+                return secondNum;
+            }
+            else if (thirdNum.CompareTo(firstNum) >= 0 && thirdNum.CompareTo(secondNum) >= 0)
+            {
+                return thirdNum;
+            }
+            return default;
+        }
     }
 }
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -11,11 +11,30 @@ namespace Generics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to \"Find Max of three numbers using generics\" program\n");
-
             MaxNumber maximum = new MaxNumber();
-            int intmax = maximum.GetMaxOfThree(26, 24,97);
-            Console.WriteLine("Maximum of three integers is: " + intmax);
+            Console.Write("Please choose an option: ");
+            Console.WriteLine("\n1. Find Max Of Three Int Number \n2. Find Max Of Three Float Number");
+
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    int intnum1 = 66, intnum2 = 78, intnum3 = 56;
+                    int intmax = maximum.GetMaxOfThree(intnum1, intnum2, intnum3);
+                    Console.WriteLine("Maximum of three integers is: " + intmax);
+                    break;
+                case 2:
+                    float floatnum1 = 26.02f, floatnum2 = 24.07f, floatnum3 = 97.78f;
+                    float floatmax = maximum.GetMaxOfThree(floatnum1, floatnum2, floatnum3);
+                    Console.WriteLine("Maximum of three integers is: " + floatmax);
+                    break;
+                default:
+                    Console.WriteLine("Please choose the correct option");
+                    break;
+                   
+            }
             Console.ReadLine();
         }
+
     }
 }
