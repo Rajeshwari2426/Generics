3fc64a510cc3c5ce9a6254e4dc7552840e4da55e
Rajeshwari2426 <rajarajeshwaribrgandi@gmail.com>
2022-03-01 18:14:15 +0530

    [Rajeshwari] adding Max of 3 Strings

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -13,7 +13,7 @@ namespace Generics
             Console.WriteLine("Welcome to \"Find Max of three numbers using generics\" program\n");
             MaxNumber maximum = new MaxNumber();
             Console.Write("Please choose an option: ");
-            Console.WriteLine("\n1. Find Max Of Three Int Number \n2. Find Max Of Three Float Number");
+            Console.WriteLine("\n1. Find Max Of Three Int Number  \n2. Find Max Of Three Float Number \n3. Find Max Of Three Strings");
 
             int option = int.Parse(Console.ReadLine());
             switch (option)
@@ -27,6 +27,11 @@ namespace Generics
                     float floatnum1 = 26.02f, floatnum2 = 24.07f, floatnum3 = 97.78f;
                     float floatmax = maximum.GetMaxOfThree(floatnum1, floatnum2, floatnum3);
                     Console.WriteLine("Maximum of three integers is: " + floatmax);
+                    break;
+                case 3:
+                    string str1 = "Jerry", str2 = "Tom", str3 = "Oswald";
+                    string stringMax = maximum.GetMaxOfThree(str1, str2, str3);
+                    Console.WriteLine("Maximum of three strings is: " + stringMax);
                     break;
                 default:
                     Console.WriteLine("Please choose the correct option");
